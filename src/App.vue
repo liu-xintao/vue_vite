977303bb436665783d580c2dbc25e4534c422dee
liuxintao <1666135248@qq.com>
2023-05-31 23:31:00 +0800

    ahha

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,3 @@
-<!--
- * @Author: liuxintao
- * @Date: 2023-05-27 09:16:25
- * @LastEditors: liuxintao
- * @LastEditTime: 2023-05-27 09:18:18
- * @FilePath: /vue_vite/src/App.vue
- * @Description: 
--->
 <template>
   <div>
     <h1>App</h1>
@@ -13,7 +5,19 @@
 </template>
 
 <script setup lang="ts">
+let a = "haha";
+console.log(a);
 
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.a {
+  width: 50px;
+  height: 50px;
+
+  .b {
+    width: 20px;
+    height: 30px;
+  }
+}
+</style>
